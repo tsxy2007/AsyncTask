864103a4b310d7dec3fac0b7c70e46e38a0a37cd
tongkuan.zhu <tongkuan.zhu@yingxiong.com>
2020-02-24 22:58:18 +0800

    123

--- Source/AsyncTask/Public/Core/ThreadCoreMacro.h
+++ Source/AsyncTask/Public/Core/ThreadCoreMacro.h
@@ -1,8 +1,27 @@
 #pragma once
 
-#include <thread>
+#include "Misc/ScopeLock.h"
+#include "Abandonable/SimpleAbandonable.h"
+#include "Async/TaskGraphInterfaces.h"
+
+
+#define DDD(...) __VA_ARGS__
+
+//一下是宏封装
+#if PLATFORM_WINDOWS
 #include <iostream>
-#include "Tickable.h"
+#include <thread>
+#define CPUThreadNumber std::thread::hardware_concurrency()
+#else
+#if PLATFORM_LINUX
+#define CPUThreadNumber 12
+#else
+#if PLATFORM_MAC
+#define CPUThreadNumber 12
+#endif
+#endif
+#endif
+
 
 #define CPUThreadNum std::Thread::hardware_concurrency()
 
@@ -57,3 +76,29 @@ USE_UE4_POOL_SYNCTASK(FSimpleDelegate::CreateLambda(__VA_ARGS__))
 
 #define SYNCTASK_STATIC(...)\
 USE_UE4_POOL_SYNCTASK(FSimpleDelegate::CreateStatic(__VA_ARGS__))
+
+
+//呼叫线程 可以设置前置任务
+#define CALL_THREAD(OtherTask,CallThreadName,InTaskDeletegate) \
+FSimpleDelegateGraphTask::CreateAndDispatchWhenReady(InTaskDeletegate,TStatId(),OtherTask,CallThreadName);
+
+#define CALL_THREAD_UOBJECT(OtherTask,CallThreadName,Object,...) \
+CALL_THREAD(OtherTask,CallThreadName,FSimpleDelegate::CreateUObject(Object,__VA_ARGS__))
+
+#define CALL_THREAD_Raw(OtherTask,CallThreadName,Object,...) \
+CALL_THREAD(OtherTask,CallThreadName,FSimpleDelegate::CreateRaw(Object,__VA_ARGS__))
+
+#define CALL_THREAD_SP(OtherTask,CallThreadName,Object,...) \
+CALL_THREAD(OtherTask,CallThreadName,FSimpleDelegate::CreateSP(Object,__VA_ARGS__))
+
+#define CALL_THREAD_Lambda(OtherTask,CallThreadName,...) \
+CALL_THREAD(OtherTask,CallThreadName,FSimpleDelegate::CreateLambda(__VA_ARGS__))
+
+#define CALL_THREAD_UFunction(OtherTask,CallThreadName,Object,...) \
+CALL_THREAD(CallThreadName,FSimpleDelegate::CreateUFunction(Object,__VA_ARGS__))
+
+#define WAITING_OTHER_THREADS_COMPLETED(EventRef) \
+FTaskGraphInterface::Get().WaitUntilTaskCompletes(EventRef);
+
+#define WAITING_OTHER_THREADS_COMPLETED_MULTI(EventRef) \
+FTaskGraphInterface::Get().WaitUntilTasksComplete(EventRef);
